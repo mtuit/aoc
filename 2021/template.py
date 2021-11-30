a2cb679eaccae5073ceed1e96fcd563ed2156fb9
Mick Tuit <Mick.Tuit@infosupport.com>
2021-11-30 18:05:41 +0100

    restructure for 2021

---
+++ 2021/template.py
@@ -0,0 +1,22 @@
+import collections
+import copy
+import itertools
+import re
+
+def read_input(file):
+    with open(file) as f: 
+        data = f.read().splitlines()
+    return data
+
+
+def solve(data):
+    result1, result2 = (None, None)
+
+    return result1, result2
+
+
+if __name__ == "__main__":
+    file = 'input/dayx.txt'
+    data = read_input(file)
+    result1, result2 = solve(data)
+    print(f"Result for part 1: {result1}\nResult for part 2: {result2}")
--- README.md
+++
@@ -1,2 +0,0 @@
-# Advent of Code 2020
-Advent of Code submissions for 2020, programmed in Python trying to balance efficiency and readability. 
